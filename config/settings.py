17b104d5688ae92dc9cd1178c3637146a7d6e848
Kiana Ghamsari <kiana.gh198@gmail.com>
2025-12-24 19:52:32 +0330

    Add Redis caching for posts list endpoint

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,17 @@ REST_FRAMEWORK = {
     )
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+
 
 
 # Database
